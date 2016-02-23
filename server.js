dc8c99a176df242b001247eb68a1c2dd036295d2
Paul Santos <paulmichael.a.santos@gmail.com>
2016-02-22 20:46:49 -0500

    set up basic server and added npm packages to server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+var express           = require('express');
+var expressHandlebars = require('express-handlebars');
+var bodyParser        = require('body-parser');
+var session           = require('express-session');
+var Sequelize         = require('sequelize');
+var mysql             = require('mysql');
+var bcrypt            = require('bcryptjs');
+var app               = express();
+
+const PORT = process.env.PORT || 8080;
+
+var sequelize = new Sequelize('Ragat_db', 'root');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/static', express.static('public'));
+app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+app.get('/', function(req, res) {
+  res.send('hello');
+});
+
+
+sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("LISTNEING!");
+  });
+});
