2cdd95339fc4f1f5f6b9b00f680f6763bc572710
unknown <you@example.com>
2016-02-24 18:37:04 -0500

    basic home layout created

---
+++ app/routes/routes.js
@@ -0,0 +1,8 @@
+
+
+module.exports.routes =  function(app) {
+
+  app.get('/', function(req,res){
+    res.render('home')
+  });
+}
--- server.js
+++ server.js
@@ -5,23 +5,22 @@ var session           = require('express-session');
 var Sequelize         = require('sequelize');
 var mysql             = require('mysql');
 var bcrypt            = require('bcryptjs');
+var routes            = require('./app/routes/routes.js')
 var app               = express();
+
+app.set('views', __dirname + '/app/views');
 
 const PORT = process.env.PORT || 8080;
 
-var sequelize = new Sequelize(process.env.DATABASE_URL);
-// var sequelize = new Sequelize('Ragat_db', 'root');
+// var sequelize = new Sequelize(process.env.DATABASE_URL);
+var sequelize = new Sequelize('Ragat_db', 'root');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/static', express.static('public'));
-app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
+app.engine('handlebars', expressHandlebars({defaultLayout: __dirname + '/app/views/layouts/main.handlebars', layoutsDir: __dirname + '/app/views/layouts'}));
 app.set('view engine', 'handlebars');
 
-
-app.get('/', function(req, res) {
-  res.send('hello');
-});
-
+routes.routes(app);
 
 sequelize.sync().then(function() {
   app.listen(PORT, function() {
