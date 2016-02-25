1a6306c5aefc78303690f614449445163cc0f3f6
unknown <you@example.com>
2016-02-25 15:38:56 -0500

    basic frontend password validation working

--- public/js/register.js
+++ public/js/register.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
-    jQuery.validator.setDefaults({
-    debug: true,
-    success: "valid"
-  });
-
-  $( "#my_form" ).validate({
-    rules: {
-      first_name: {
-        required: true
-      }
-    }
-  });
+jQuery.validator.setDefaults({
+  debug: true,
+  success: "valid"
 });
+
+$("#myform").validate({
+  rules:{
+    password: {
+      required: true
+    },
+    password_check: {
+      required: true
+    }
+  }
+});
+
+}); 
