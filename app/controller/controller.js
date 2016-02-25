39013210869341ca7ea013edbbd61eaad21257fb
Paul Santos <paulmichael.a.santos@gmail.com>
2016-02-24 20:27:41 -0500

    added route for the register page and added a render controller for the register page

---
+++ app/controller/controller.js
@@ -0,0 +1,10 @@
+/* ============================================================================================
+    RENDERS
+  ==================================================================================*/
+exports.home = function(req, res, next) {
+  res.render('home');
+}
+
+exports.register = function(req, res, next) {
+  res.render('register');
+}
