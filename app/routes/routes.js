7c0218441d7fc747ba68d8f3a15aaf63fc1a1822
Paul Santos <paulmichael.a.santos@gmail.com>
2016-02-27 11:35:36 -0500

    fixed merge conflict?

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -6,11 +6,10 @@ module.exports.routes =  function(app) {
 
   app.get('/register', renders.register);
 
-<<<<<<< HEAD
   app.get('/activities', renders.activities);
 
-  app.get('/test', renders.test)
-=======
+  app.get('/test', renders.test);
+
   app.post('/register/addUser', renders.newUser, renders.homeRedirect);
->>>>>>> 6265a4f7039bf73af206d48cacbf41be716c1fc1
+
 }
